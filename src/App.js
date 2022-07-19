b91296de757fc988c0cad487cb15b9b284f0db6f
martiwg <martiwg.gg@gmail.com>
2022-07-19 17:13:02 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/home/Home'
-import About from './pages/about/About'
 import Work from './pages/work/Work'
-import Contact from './pages/contact/Contact'
 
 const App = () => {
   return(
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/work" element={<Work />} />
-        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   )
