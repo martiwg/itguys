6d9b51d1ea23f080acb08b208bf1d9e89a65bad7
pcoderch <93677392+pcoderch@users.noreply.github.com>
2022-07-19 18:01:09 +0200

    implementing contact section

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,5 +1,5 @@
 import Navbar from '../../components/navbar/Navbar'
-
+import fiverImage from '../../assets/images/fiver.png'
 import styles from './Contact.module.css'
 
 const Contact = () => {
@@ -7,9 +7,53 @@ const Contact = () => {
     <div className='globalContainer'>
       <div className='globalWrapper'>
         <Navbar />
+        <ContactUs />
       </div>
     </div>
   )
 }
+
+const ContactUs = () => {
+    return(
+      // <div className={styles.container}>
+      //   <a href="https://es.fiverr.com/itguyss
+      //   " target="_blank" rel="noopener noreferrer">
+      //     <img src={fiverImage} className={styles.fiverImage} alt="fiver" />
+      //   </a>
+      //   <a href="https://es.fiverr.com/itguyss
+      //   " target="_blank" rel="noopener noreferrer">
+      //     <img src={fiverImage} className={styles.fiverImage} alt="fiver" />
+      //   </a>
+      // </div>
+
+      <div className={styles.container}>
+        <div className={styles.secondContainer}>
+          <div className={styles.leftContainer}>
+            <div className={styles.leftContainerTitle}>
+              <h1>Contact</h1>
+            </div>
+            <div className={styles.leftContainerText}>
+              <p>
+                Si quieres contactar conmigo, puedes hacerlo a través de cualquiera de los siguientes medios:
+              </p>
+              <p>
+                <a href="mailto:
+                ">
+                  <img src={fiverImage} className={styles.fiverImage} alt="fiver" />
+                </a>
+                <a href="mailto:
+                ">
+                  <img src={fiverImage} className={styles.fiverImage} alt="fiver" />
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className={styles.rightContainer}>
+            </div>
+        </div>
+      </div>
+    )
+}
+
 
 export default Contact
