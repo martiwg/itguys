952c757c51cb195257db04834c1906631e8252bd
martiwg <martiwg.gg@gmail.com>
2022-07-19 15:13:09 +0200

    added path components and boilerplate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Work from './pages/work/Work'
+
 const App = () => {
   return(
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/work" element={<Work />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/work/Work.js
@@ -0,0 +1,11 @@
+import styles from './Work.module.css'
+
+const Work = () => {
+  return(
+    <div>
+      <h1>Work</h1>
+    </div>
+  )
+}
+
+export default Work
