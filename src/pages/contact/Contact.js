29897f9f1f9a44b00dbdfb20cfd266828a496974
pcoderch <93677392+pcoderch@users.noreply.github.com>
2022-07-19 15:50:40 +0200

    contact added

---
+++ src/pages/contact/Contact.js
@@ -0,0 +1,13 @@
+import styles from './Contact.module.css'
+
+const About = () => {
+  return(
+    <div className='globalContainer'>
+      <div className='globalWrapper'>
+        <h1>Contact</h1>
+      </div>
+    </div>
+  )
+}
+
+export default About
