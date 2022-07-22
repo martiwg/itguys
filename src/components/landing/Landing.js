f26d26457ce5fe5cf7d054ded377bcb8dc801215
martiwg <martiwg.gg@gmail.com>
2022-07-23 00:51:22 +0200

    minor changes

---
+++ src/components/landing/Landing.js
@@ -0,0 +1,13 @@
+import styles from './Landing.module.css'
+
+const Landing = () => {
+  return(
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Landing
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,6 +4,7 @@ import { logEvent } from 'firebase/analytics'
 import { analytics } from '../../firebaseConfig'
 
 import Navbar from '../../components/navbar/Navbar'
+import Landing from '../../components/landing/Landing'
 import Contact from '../../components/contact/Contact'
 
 import styles from './Home.module.css'
@@ -20,6 +21,7 @@ const Home = () => {
     <div className='globalContainer'>
       <div className='globalWrapper'>
         <Navbar />
+        <Landing />
         <Contact />
       </div>
     </div>
